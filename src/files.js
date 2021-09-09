a28dfd7379e06305e48d99ce847a8e0ff6763f8d
Sven Fehler <s.fehler@pbsnetwork.eu>
2021-09-09 12:15:33 +0200

    files stuff

--- src/files.js
+++ src/files.js
@@ -1,9 +1,21 @@
-const { filesystem } = require("svcorelib");
-const { join, resolve } = require("path");
-const { ensureDir, writeFile, mkdir, copyFile } = require("fs-extra");
+const { filesystem, reserialize } = require("svcorelib");
+const { resolve } = require("path");
+const { ensureDir, writeFile, copyFile, readFile } = require("fs-extra");
 const { hide } = require("hidefile");
 
 const packageJSON = require("../package.json");
+
+/** @typedef {import("svcorelib").JSONCompatible} JSONCompatible */
+/** @typedef {import("../.notifier/properties.json")} PropJsonFile */
+
+
+/**
+ * Contains all file paths
+ * @readonly
+ */
+const paths = Object.freeze({
+    propJson: resolve("./.notifier/properties.json"),
+});
 
 
 /**
@@ -26,23 +38,22 @@ function initDirs()
                 stage = "creating ./notifier";
                 await ensureDir("./notifier");
 
-                stage = "hiding directory ./notifier"
-                const notifierDir = await hidePath("./notifier");
+                stage = "hiding directory ./notifier";
+                await hidePath("./notifier");
 
-                stage = "creating ./.notifier/properties.json from template"
-                await writeFile(join(notifierDir, "properties.json"), getPropJsonTemplate());
+                stage = `creating ${paths.propJson} from template`;
+                await writeFile(paths.propJson, getPropJsonTemplate());
 
                 stage = "copying ./www/favicon.png to ./assets/example.png";
                 await copyFile("./www/favicon.png", "./assets/example.png");
             }
 
             stage = "checking if properties.json exists";
-            if(!(await filesystem.exists("./.notifier/properties.json")))
+            if(!(await filesystem.exists(paths.propJson)))
             {
                 stage = "creating properties.json";
                 
-                const propJPath = resolve("./.notifier/properties.json");
-                await writeFile(propJPath, getPropJsonTemplate());
+                createPropJsonFile();
             }
 
             stage = "(done)";
@@ -90,7 +101,83 @@ function getPropJsonTemplate()
     return JSON.stringify(JSON.parse(propJTemp.join("")), undefined, 4);
 }
 
+/**
+ * Creates the properties.json file
+ * @returns {Promise<void, (Error | string)>}
+ */
+function createPropJsonFile()
+{
+    return new Promise(async (res, rej) => {
+        try
+        {
+            const propJPath = resolve("./.notifier/properties.json");
+            await writeFile(propJPath, getPropJsonTemplate());
+
+            return res();
+        }
+        catch(err)
+        {
+            return rej(err);
+        }
+    });
+}
+
+/**
+ * Reads the properties.json file and returns it.  
+ * Also creates the file if it doesn't exist yet.
+ * @returns {Promise<PropJsonFile, (Error | string)>}
+ */
+function getProperties()
+{
+    return new Promise(async (res, rej) => {
+        try
+        {
+            if(!(await filesystem.exists("./.notifier/properties.json")))
+                createPropJsonFile();
+
+            const fileContent = await readFile(paths.propJson);
+
+            const parsed = JSON.parse(fileContent.toString());
+
+            return res(parsed);
+        }
+        catch(err)
+        {
+            return rej(new Error(`Error while reading properties.json file: ${err}`));
+        }
+    });
+}
+
+/**
+ * Sets a property of the properties.json file with the given `key` to the given JSON-compatible `value`
+ * @param {keyof(PropJsonFile)} key
+ * @param {JSONCompatible} value
+ * @returns {Promise<void, (Error | string)>}
+ */
+function setProperty(key, value)
+{
+    return new Promise(async (res, rej) => {
+        try
+        {
+            const oldContent = await getProperties();
+
+            const newContent = reserialize(oldContent);
+            newContent[key] = value;
+
+            await writeFile(paths.propJson, JSON.stringify(newContent, undefined, 4));
+
+            return res();
+        }
+        catch(err)
+        {
+            return rej(new Error(`Error while setting properties.json property with key '${key}' to value '${value}': ${err}`));
+        }
+    });
+}
+
 module.exports = {
     initDirs,
-    hidePath
+    hidePath,
+    getProperties,
+    setProperty,
 };
